bec8bc79d09b8e7dedb2c71e3b4a256ce2f016f1
marcinm312 <marcinm312@users.noreply.github.com>
2020-06-07 19:58:15 +0200

    refactoring

--- rest-service.py
+++ rest-service.py
@@ -2,35 +2,40 @@ import json
 from flask import jsonify
 from flask import request
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.errorhandler(404)
 def not_found(error=None):
     message = {
-            'status': 404,
-            'message': 'Not Found: ' + request.url,
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
     }
     resp = jsonify(message)
     resp.status_code = 404
 
     return resp
 
+
 @app.route('/')
 def api_root():
     return 'Welcome to my REST service'
 
+
 @app.route('/books')
 def api_books():
-    result=[]
+    result = []
     with open('books.json') as json_data:
         data = json.load(json_data)
         for book in data['books']:
             result.append(book['isbn'])
     return jsonify(result)
 
+
 @app.route('/title/<search>')
 def api_title(search):
-    result=[]
+    result = []
     with open('books.json') as json_data:
         data = json.load(json_data)
         for book in data['books']:
@@ -38,31 +43,33 @@ def api_title(search):
                 result.append(book['title'])
     return jsonify(result)
 
+
 @app.route('/book/<isbn>')
 def api_book_isbn(isbn):
-    result=''
+    result = ''
     with open('books.json') as json_data:
         data = json.load(json_data)
         for book in data['books']:
             if book['isbn'] == isbn:
                 result = jsonify(book)
                 return result
-        if result=='':
+        if result == '':
             return not_found()
 
-@app.route('/book/<isbn>', methods = ['PUT'])
+
+@app.route('/book/<isbn>', methods=['PUT'])
 def api_update_book(isbn):
-    title = request.args.get('title', default = '*', type = str)
+    title = request.args.get('title', default='*', type=str)
     with open("books.json", "r+") as jsonFile:
         data = json.load(jsonFile)
         for book in data['books']:
             if book['isbn'] == isbn:
-                tmp = book["title"]
                 book["title"] = str(title)
                 jsonFile.seek(0)  # rewind
                 json.dump(data, jsonFile)
                 jsonFile.truncate()
     return 'Tytul w ksiazce o ISBN: ' + isbn + ' zmieniony na: ' + title
 
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
